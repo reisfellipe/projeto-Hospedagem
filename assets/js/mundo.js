304dfb35108b6d5615cb47775e91da77cbfb11ae
Fellipe Reis <reisfellipe11@gmail.com>
2024-03-21 01:14:04 -0300

    its already

---
+++ assets/js/mundo.js
@@ -0,0 +1,6 @@
+const quadro = document.querySelector('.quadro');
+const toggleButton = document.getElementById('botao');
+
+botao.addEventListener('click', ()=>{
+    quadro.classList.toggle('ativo');
+});
